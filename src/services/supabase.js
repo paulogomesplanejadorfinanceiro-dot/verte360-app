67a87048c83b9535df2dbae905edfef3b28aa20a
paulogomesplanejadorfinanceiro-dot <paulogomesplanejadorfinanceiro@gmail.com>
2026-04-20 22:37:41 -0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://pnurseudpiyyosulmwrf.supabase.co";
+const supabaseAnonKey = "sb_publishable_YaOLy2InC7wJuVNjqvg8Lw_3pRLvmAY";
 
-const supabaseAnonKey = "sb_publishable_YaOLy2InC7wJuVNjqvg8Lw_3pRLwnAY";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
