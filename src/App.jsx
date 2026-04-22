534ce7a79b82c989e9beb45fa3bb7852f6f5ca5e
paulogomesplanejadorfinanceiro-dot <paulogomesplanejadorfinanceiro@gmail.com>
2026-04-22 11:54:50 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import Dashboard from "./pages/Dashboard";
+import Sidebar from "./components/Sidebar";
 import "./app.css";
 
 export default function App() {
-  return <Dashboard />;
+  return (
+    <div style={styles.container}>
+      <Sidebar />
+
+      <div style={styles.content}>
+        <Dashboard />
+      </div>
+    </div>
+  );
 }
+
+const styles = {
+  container: {
+    display: "flex",
+  },
+
+  content: {
+    flex: 1,
+  },
+};
